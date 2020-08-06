30dcf8ede7a78c1f6b1da2d886b4f27e6400ec93
baselrabia <baselrabia@gmail.com>
2020-08-07 01:27:19 +0200

    03-authentication-with-sanctum-(formerly-airlock)

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,29 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div >
+        {{user}}
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
+        data(){
+            return {
+                user:null
+            }
+        },
+
+        methods: {
+            async getUser () {
+                let response = await axios.get('/api/user')
+
+                this.user = response.data
+            }
+        },
+
         mounted() {
-            console.log('Component mounted.')
+            this.getUser()
         }
     }
 </script>
