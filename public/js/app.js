a61df64051707ceef93b1a180789637cad9f1b4c
baselrabia <baselrabia@gmail.com>
2020-08-11 05:56:49 +0200

    30-updating-like-counts-on-retweets-and-quotes

--- public/js/app.js
+++ public/js/app.js
@@ -60415,6 +60415,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
+/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_2__);
 
 
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
@@ -60432,6 +60434,7 @@ function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.it
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
@@ -60462,6 +60465,10 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
       state.tweets = state.tweets.map(function (t) {
         if (t.id === id) {
           t.likes_count = count;
+        }
+
+        if (Object(lodash__WEBPACK_IMPORTED_MODULE_2__["get"])(t.original_tweet, 'id') === id) {
+          t.original_tweet.likes_count = count;
         }
 
         return t;
--- resources/js/store/timeline.js
+++ resources/js/store/timeline.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import {get} from "lodash"
 export default {
     namespaced: true,
 
@@ -25,6 +25,9 @@ export default {
             state.tweets= state.tweets.map((t)=>{
                 if(t.id === id ){
                     t.likes_count = count
+                }
+                if(get(t.original_tweet,'id') === id){
+                    t.original_tweet.likes_count = count
                 }
                 return t
             })
