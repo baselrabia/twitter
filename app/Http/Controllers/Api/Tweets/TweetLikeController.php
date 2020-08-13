a7ab24e8d65e797696ef1e62c67f87823e79b0ee
baselrabia <baselrabia@gmail.com>
2020-08-13 21:07:56 +0200

    63-tweet-like-notification

--- app/Http/Controllers/Api/Tweets/TweetLikeController.php
+++ app/Http/Controllers/Api/Tweets/TweetLikeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Tweets;
 
 use App\Events\Tweets\TweetLikesWereUpdated;
 use App\Http\Controllers\Controller;
+use App\Notifications\Tweets\TweetLiked;
 use App\Tweet;
 use Illuminate\Http\Request;
 
@@ -23,6 +24,10 @@ class TweetLikeController extends Controller
         $request->user()->likes()->create([
             'tweet_id'=> $tweet->id
             ]);
+
+        if( $tweet->user()->id !== $tweet->user_id){
+            $tweet->user->notify(new TweetLiked($request->user,$tweet));
+        }
 
         broadcast(new TweetLikesWereUpdated($request->user(),$tweet));
 
---
+++ app/Notifications/Tweets/TweetLiked.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications\Tweets;
+
+use App\Http\Resources\TweetResource;
+use App\Http\Resources\UserResource;
+use App\Tweet;
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class TweetLiked extends Notification
+{
+    use Queueable;
+
+    protected $user;
+
+    protected $tweet;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+
+    public function __construct(User $user, Tweet $tweet)
+    {
+        $this->user = $user;
+        $this->tweet = $tweet;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'user' => new UserResource($this->user),
+            'tweet' =>  new TweetResource($this->tweet),
+        ];
+    }
+}
