4a0d34e37d664fb02777df8e62f2392ce2efa863
baselrabia <baselrabia@gmail.com>
2020-08-14 03:12:48 +0200

    71-tweaking-performance-for-notification-tweet-loading

--- app/Http/Controllers/Api/Tweets/TweetController.php
+++ app/Http/Controllers/Api/Tweets/TweetController.php
@@ -20,7 +20,20 @@ class TweetController extends Controller
 
     public function index(Request $request)
     {
-        return new TweetCollection(Tweet::find(explode(',',$request->ids)));
+        $tweets = Tweet::with([
+                'user',
+                'likes',
+                'retweets',
+                'replies',
+                'media.baseMedia',
+                'originalTweet.user',
+                'originalTweet.likes',
+                'originalTweet.retweets',
+                'originalTweet.media.baseMedia',
+            ])
+            ->find(explode(',',$request->ids);
+            
+            return new TweetCollection($tweets));
     }
 
     public function store(TweetStoreRequest $request)
