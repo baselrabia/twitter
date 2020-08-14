f1c04772a6f2dbe8715251ba8a40a6c9f92ab068
baselrabia <baselrabia@gmail.com>
2020-08-14 03:42:14 +0200

    73-extracting-entities-with-regular-expressions

--- app/Tweet.php
+++ app/Tweet.php
@@ -9,6 +9,17 @@ class Tweet extends Model
 {
     protected $guarded = [];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function (Tweet $tweet){
+            preg_match_all('/(?!\s)#([A-Za-z]\w*)\b/', $tweet->body, $matches, PREG_OFFSET_CAPTURE);
+
+            return $matches;
+        });
+    }
+
     public function scopeParent(Builder $builder)
     {
        return $builder->whereNull('parent_id');
